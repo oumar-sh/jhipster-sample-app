f63330c1908390d1320e143207bb0b4f3823b6a7
Oumar Sharif DAMBABA <oumarsharif@gmail.com>
2021-03-12 23:08:38 +0100

    Initial version of react generated by JHipster-6.10.5

---
+++ app1/src/main/java/com/oumar/react/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.oumar.react.repository;
+
+import com.oumar.react.domain.Authority;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String> {
+}
---
+++ app1/src/main/java/com/oumar/react/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.oumar.react.service.dto;
